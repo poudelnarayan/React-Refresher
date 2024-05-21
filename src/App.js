d863b5f08f98af1e2a1bb2d5189bc0d0df44abe1
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-21 13:47:26 +0545

    react-update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
+  // return React.createElement("h1", "A React App!"); // This is how JSX is compiled
   return <h1>A React App!</h1>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.renderer(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
