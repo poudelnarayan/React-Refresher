db25c2f1bfd2489ecd3a9abbf1629c0c0aa34cb2
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 00:18:19 +0545

    refactored

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import GoalList from "./components/GoalList";
+
+import "./App.css";
 
 const App = () => {
   // return React.createElement("h1", "A React App!"); // This is how JSX is compiled
-  return <h1>A React App!</h1>;
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
+
+// class App extends React.Componene {
+//   render() {
+//     return <h1 title="This works">A React App!</h1>;
+//   }
+// }
 
 export default App;
---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish Cours</li>
+      <li>Lean all about the couse topic</li>
+      <li>lean MEARN</li>
+    </ul>
+  );
+};
+
+export default GoalList;
