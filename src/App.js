4315c5c2d900519f3bd38d15b9c5eef24cd6c7b7
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 00:54:03 +0545

    using props to pass data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import GoalList from "./components/GoalList";
 import "./App.css";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the course" },
+    { id: "cg2", text: "Learn all about the course main topic" },
+    { id: "cg3", text: "Help other students in the course Q&A" },
+  ];
   // return React.createElement("h1", "A React App!"); // This is how JSX is compiled
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals = {courseGoals} />
     </div>
   );
 };
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import './GoalList.css';
+import "./GoalList.css";
 
-const GoalList = () => {
+const GoalList = (props) => {
   return (
     <ul className="goal-list">
-      <li>Finish Cours</li>
-      <li>Lean all about the couse topic</li>
-      <li>lean MEARN</li>
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
     </ul>
   );
 };
